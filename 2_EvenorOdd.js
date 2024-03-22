b1d4995bf1e55614973d1a3d15fde6283ffab952
steven <stevenlw12@gmail.com>
2024-03-22 19:45:43 +0700

    tantangan coding 2

---
+++ 2_EvenorOdd.js
@@ -0,0 +1,15 @@
+// tantangan coding 2/365
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+// https://github.com/stevenlauwira/tantangan-coding-harian
+
+// function evenOrOdd(number) {
+//   if(number % 2===0){
+//     return "Even";
+//   }else{
+//     return "Odd";
+//   }
+// }
+
+evenOrOdd=(number) =>number %2===0 ? "Even" :"Odd";
+
+console.log(evenOrOdd(2));
